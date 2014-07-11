faa4302053b5e48e3e2be2986530e229ff5d1d32
Arnaud BRETON <arnaud@videonot.es>
2014-07-12 01:04:35 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	clientInterface "github.com/yoed/yoed-client-interface"
+	"net/http"
+	"net/url"
+	"log"
+	"fmt"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type YoBackYoedClient struct {
+	Config *YoBackYoedClientConfig
+}
+
+type YoBackYoedClientConfig struct {
+	clientInterface.AbstractYoedClientConfig
+	ApiKey string `json:"apiKey"`
+}
+
+func (c *YoBackYoedClient) loadConfig(configPath string) (*YoBackYoedClientConfig, error) {
+
+	configFile, err := os.Open(configPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	configJson, err := ioutil.ReadAll(configFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config := &YoBackYoedClientConfig{}
+
+	if err := json.Unmarshal(configJson, config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func (c *YoBackYoedClient) Handle(username string) {
+	resp, err := http.PostForm("http://api.justyo.co/yo/", url.Values{
+		"api_token": {c.Config.ApiKey},
+		"username":  {username},
+	})
+
+	log.Printf("Yo-ing back %s", username)
+
+	if err != nil {
+		log.Printf("yobackHandler: %s", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	log.Printf("yobackHandler: %s", resp.Status)
+
+	if body, err := ioutil.ReadAll(resp.Body); err != nil {
+		log.Printf("yobackHandler: %s", err)
+	} else {
+		log.Printf("yobackHandler: %s", string(body))
+	}
+}
+
+func (c *YoBackYoedClient) GetConfig() (*clientInterface.AbstractYoedClientConfig) {
+	return &clientInterface.AbstractYoedClientConfig{Listen:c.Config.Listen, ServerUrl: c.Config.ServerUrl}
+}
+
+func NewYoBackYoedClient() (*YoBackYoedClient, error) {
+	c := &YoBackYoedClient{}
+	config, err := c.loadConfig("./config.json")
+
+	if err != nil {
+		panic(fmt.Sprintf("failed loading config: %s", err))
+	}
+
+	c.Config = config
+
+	return c, nil
+}
+
+func main() {
+	c, _ := NewYoBackYoedClient()
+
+	clientInterface.Run(c)
+}
